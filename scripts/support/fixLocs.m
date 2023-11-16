ecc58c7286071c7ac5054a2319a4c8718aa9a277
PINE-Lab <northeastern.pinelab@gmail.com>
2023-11-16 13:22:59 -0800

    add fixLocs

---
+++ scripts/support/fixLocs.m
@@ -0,0 +1,28 @@
+function locs = fixLocs(origLocs, happeDir)
+    locs_1020 = readlocs([happeDir filesep 'Packages' filesep 'eeglab2022.0' ...
+        filesep 'sample_locs' filesep 'Standard-10-20-Cap81.locs']) ;
+    defaultChanIDs = {locs_1020.labels} ;
+    locs = struct('labels', [], 'type', [], 'theta', [], 'radius', [], ...
+        'X', [], 'Y', [], 'Z', [], 'sph_theta', [], 'sph_phi', [], ...
+        'urchan', [], 'ref', []) ;
+        % 'sph_radius', [], 'urchan', [], 'ref', []) ;
+
+    for currChan = 1:size(origLocs, 2)
+        try
+            chanIndx = find(strcmp(defaultChanIDs, ...
+                origLocs{currChan})) ;
+            locs(currChan).labels = origLocs{currChan} ;
+            locs(currChan).theta = locs_1020(chanIndx).theta ;
+            locs(currChan).radius = locs_1020(chanIndx).radius ;
+            locs(currChan).X = locs_1020(chanIndx).X ;
+            locs(currChan).Y = locs_1020(chanIndx).Y ;
+            locs(currChan).Z = locs_1020(chanIndx).Z ;
+            locs(currChan).sph_theta = locs_1020(chanIndx).sph_theta ;
+            locs(currChan).sph_phi = locs_1020(chanIndx).sph_phi ;
+            % locs(currChan).sph_radius = locs_1020(chanIndx).sph_radius ;
+            locs(currChan).type = locs_1020(chanIndx).type ; 
+            locs(currChan).urchan = currChan ;
+        catch ME
+        end
+    end
+end
